e599203c48fca007f97ce1ec68e8de615276309a
usbharu <i@usbharu.dev>
2025-07-13 13:33:32 +0900

    native buildでも起動できるように

--- src/main/kotlin/dev/usbharu/todouser2/TodoUser2Application.kt
+++ src/main/kotlin/dev/usbharu/todouser2/TodoUser2Application.kt
@@ -1,14 +1,28 @@
 package dev.usbharu.todouser2
 
 import dev.usbharu.todouser2.users.UserId
+import org.springframework.aot.hint.MemberCategory
+import org.springframework.aot.hint.RuntimeHints
+import org.springframework.aot.hint.RuntimeHintsRegistrar
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.context.properties.ConfigurationPropertiesScan
 import org.springframework.boot.context.properties.EnableConfigurationProperties
 import org.springframework.boot.runApplication
+import org.springframework.context.annotation.ImportRuntimeHints
+import kotlin.jvm.java
 
 @SpringBootApplication
 @ConfigurationPropertiesScan
-class TodoUser2Application
+@ImportRuntimeHints(TodoUser2Application.Hints::class)
+class TodoUser2Application{
+    class Hints : RuntimeHintsRegistrar {
+        override fun registerHints(hints: RuntimeHints, classLoader: ClassLoader?) {
+            hints.reflection().registerTypeIfPresent(classLoader, UserId::class.java.name) {
+                it.withMembers(*MemberCategory.entries.toTypedArray())
+            }
+        }
+    }
+}
 
 fun main(args: Array<String>) {
     runApplication<TodoUser2Application>(*args)
