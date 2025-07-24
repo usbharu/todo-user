b29aa2efab7dc96e1aef5cf130b93496d7a2bb45
usbharu <i@usbharu.dev>
2025-07-24 18:42:54 +0900

    fix: jwtのsubjectがusernameになっていた問題を修正

--- src/main/kotlin/dev/usbharu/todouser/domain/users/UserId.kt
+++ src/main/kotlin/dev/usbharu/todouser/domain/users/UserId.kt
@@ -9,6 +9,10 @@ import java.util.*
 @JvmInline
 value class UserId(val id: UUID) {
     companion object {
+        fun fromString(subject: String): UserId {
+            return UserId(UUID.fromString(subject))
+        }
+
         val ZERO = UUID.fromString("0000000-0000-0000-0000-000000000000")
     }
 }
