7283bb69966b03eb174c1538d94d721339fffca0
usbharu <i@usbharu.dev>
2025-07-25 00:15:46 +0900

    docs: わかりにくいオブジェクトにドキュメントを追加

--- src/main/kotlin/dev/usbharu/todouser/application/users/User.kt
+++ src/main/kotlin/dev/usbharu/todouser/application/users/User.kt
@@ -1,8 +1,10 @@
 package dev.usbharu.todouser.application.users
 
 import dev.usbharu.todouser.domain.users.UserId
+import io.swagger.v3.oas.annotations.media.Schema
 
 /**
  * ユーザー情報
  */
+@Schema(description = "ログイン中のユーザー以外のユーザーの情報")
 open class User(val username: String, val userId: UserId)
